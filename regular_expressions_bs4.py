f1d8205f8ef57f5aed54ccfd305c6a65648da77a
Techneshaman <witkowski.m.t@gmail.com>
2017-03-24 22:24:37 +0100

    Learning that Polish National Bank's website is ANCIENT and badly written.

--- regular_expressions_bs4.py
+++ regular_expressions_bs4.py
@@ -11,14 +11,31 @@ import re
 # print("\nAfter:\n")
 # print(re.sub(regexp2, "", text1))
 
-url = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
+url = 'http://www.nbp.pl/home.aspx?f=/kursy/kursya.html'
 response = urllib3.connection_from_url(url)
 html = response.urlopen('GET', url)
 soup = bs4.BeautifulSoup(html.data, 'html.parser')
 soup_title = soup.title
-soup_links = soup.find_all('a')
+soup_tables = soup.find_all('table')
+
+
+class HTMLTableParser():
+    def __init__(self, input_table):
+        self.input_table = input_table
+
+    def get_table(self):
+        print("NEW TABLE")
+        table_soup = bs4.BeautifulSoup(str(self.input_table), 'html.parser')
+        rows_list_iter = table_soup.find_all('tr')
+        print('Rows in table:', len(list(rows_list_iter)))
+        for item in rows_list_iter:
+            print("NEXT LINE")
+            for child in item.children:
+                if child !='\n':
+                    print("NEXT CELL:", child.text)
+        print("\n\n\n")
 
 print(soup_title)
-for item in soup_links:
-    print(item)
-
+for item in soup_tables:
+    parser = HTMLTableParser(item)
+    parser.get_table()
