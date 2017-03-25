5792b06260d522e131e8b130832df69af1e3614a
Techneshaman <witkowski.m.t@gmail.com>
2017-03-25 22:32:10 +0100

    So that's how subplot positions work

--- random_playground.py
+++ random_playground.py
@@ -6,20 +6,20 @@ import matplotlib.pyplot as plotter
 
 data = [1,2,3,4,5]
 
-print(data)
+print("Input list:", data)
 random.shuffle(data)
-print(data)
+print("Shuffled list:", data)
 
 data2 = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-print(data2)
+print('Input list of lists:', data2)
 random.shuffle(data2)
-print(data2)
+print('Shuffled list of lists:', data2)
 
 # looks like documentation is wrong? random.choices doesn't exist. Use random.sample instead or sth.
 
-print(random.choice(data))
-print(random.choice(data2))
-print(random.sample(range(100), k=2))
+print("Random element of input list:", random.choice(data))
+print("Random element of input list of lists:", random.choice(data2))
+print('K random numbers from selected range:', random.sample(range(100), k=2))
 
 # some examples for random drawings from different distributions. NICE
 
@@ -34,7 +34,7 @@ while i < sample_size:
     i += 1
     normal_draw = random.gauss(0,1)
     uniform_draw = random.uniform(0,1)
-    triangular_draw = random.triangular(0,1,0.9)
+    triangular_draw = random.triangular(0,1,0.8)
     lognormal_draw = random.lognormvariate(0,1)
     samples['normal'].append(normal_draw)
     samples['uniform'].append(uniform_draw)
@@ -43,6 +43,27 @@ while i < sample_size:
 
 positions = [221,222,223,224]
 i = 0
+for item in samples:
+    plotter.subplot(positions[i])
+    plotter.hist(samples[item], bins=19)
+    plotter.title(item)
+    i += 1
+
+plotter.show()
+
+# actually that's how positions work if I don't want them to be too fancy
+
+grid_height = 2
+grid_width = 2
+amount_of_charts = 4
+
+positions = []
+for i in range (1, amount_of_charts+1):
+    chart_position = grid_height * 100 + grid_width * 10 + i
+    positions.append(chart_position)
+
+i = 0
+plotter.figure()
 for item in samples:
     plotter.subplot(positions[i])
     plotter.hist(samples[item], bins=19)
